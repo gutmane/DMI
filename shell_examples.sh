4a076c5a08eca6f8b5cdb73c1e5bb5f5ab664c8c
krisypon@inbox.lv <gutmane.agnese@inbox.lv>
2017-09-27 16:12:30 +0300

    20170927_16_10

--- shell_examples.sh
+++ shell_examples.sh
@@ -1,22 +1,56 @@
 
 #!/bin/bash
 
+#7.piemers
+skaitliska_vertiba='expr 2 + 2'
+echo "Summas vertiba : "$skaitliska_vertiba
+echo "Summas vertiba : $skaitliska_vertiba"
 
+skaitiska_vertiba=`expr 2 + 2`
+echo "Summas vertiba : "$skaitliska_vertiba
+echo "
 
+#6.piemers
+#echo $*
+#echo "----------------"
+#kartas_numurs=1
+#for arguments in $*
+#do
+#    echo $kartas_numurs" . arguments - " $arguments
+#    kartas_numurs=$kartas_numurs+1 
+#done
+
+#5.piemers
+#echo "Skriptam nodoto argumentu skaits : " $#
+#echo "Argumentu sarakts (attelosana/ grupesana veids 1) : " $*
+#echo "Argumentu sarakts (attelosana/grupesana veids 2) : "$@
+#echo "Pirma argumenta vertiba : " $1
+#echo "Otra argumenta vertiba : " $2
+#echo $1$2
+
+#4.piemers
+#echo "Izpildama skripta faila nosaukums : " $0
+#echo $n
+#echo "Skriptam nodoto argumentu skaits : " $#
+#echo "Argumentu sarakts (attelosana/ grupesana veids 1) : " $*
+#ech "Argumentu sarakts (attelosana/grupesana veids 2) : "$@
+#echo "Ieprieksejas komandas izpildes rezultats : " $?
+#echo "Skripta izpildei pieskirtais numurs : " $$
+#echo $!
 
 
 #3.piemers
-NAME="Vards Uzvards"
-echo $NAME
-Name="Uzvards Vards"
-echo $NAME
+#NAME="Vards Uzvards"
+#echo $NAME
+#Name="Uzvards Vards"
+#echo $NAME
 
 #2.piemers
-NAME="Vards Uzvards"
-readonly NAME
-echo $NAME
-Name="Uzvards Vards"
-echo $NAME
+#NAME="Vards Uzvards"
+#readonly NAME
+#echo $NAME
+#Name="Uzvards Vards"
+#echo $NAME
 
 
 #1.piemers
