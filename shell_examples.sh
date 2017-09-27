a630243c048d4889809082d70d4be6e3cdf8afd9
krisypon@inbox.lv <gutmane.agnese@inbox.lv>
2017-09-27 14:15:18 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+
+
+
+
+#3.piemers
+NAME="Vards Uzvards"
+echo $NAME
+Name="Uzvards Vards"
+echo $NAME
+
+#2.piemers
+NAME="Vards Uzvards"
+readonly NAME
+echo $NAME
+Name="Uzvards Vards"
+echo $NAME
+
+
+#1.piemers
+#NAME="Vards Uzvards"
+#echo $NAME
+
+#0.piemers
+#history > history_20170927.txt
