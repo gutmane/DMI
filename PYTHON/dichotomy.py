0fad6950bae29cb147ee52e2f6b23b88fa777ccc
Agnese <gutmane.agnese@inbox.lv>
2018-02-02 14:59:19 +0200

    https://github.com/gutmane/DMI

--- PYTHON/dichotomy.py
+++ PYTHON/dichotomy.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+#print formāti
+# http://www.cplusplus.com/refrence/cstdio/printf
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 def mans_sinuss(x):
     k = 0
@@ -13,6 +15,7 @@ def mans_sinuss(x):
         a = a * R
         S = S + a
     return S
+
 a = 1.57
 b = 4.71
 x = np.arange(a,b,0.01)
@@ -20,26 +23,28 @@ y = mans_sinuss(x)
 plt.plot(x,y)
 plt.grid()
 #plt.show()
-delta_x = 1.e-3 # 0.001 ir tas pats, kas 1.e-3
+
+delta_x = 1.e-3 # 0.001 ir tas pats kas 1.e-3
 funa = mans_sinuss(a)
 funb = mans_sinuss(b)
 if funa * funb > 0:
-    print "[%.2f,%.2f] intervala saknu nav"%(a,b)
-    print "vai saja intervala ir paru saknu skaits"
+    print "[%.2f,%.2f] intervālā sakņu nav"%(a,b)
+    print "vai šajā intervālā ir pāru sakņu skaits"
     exit()
-print "Turpinajums, kad sakne ir:"    
-print "Vertibas intervala galapunktos - ",
+print "Turpinajums, kad sakne ir:"
+print "Vertības intervāla galapunktos - ",
 print "f(%.2f)=%.2f un f(%.2f)=%.2f"%(a,funa,b,funb)
+
 k = 0
 while b-a > delta_x:
     k = k + 1
     x = (a+b)/2
-    funx = mans_sinuss (x)
+    funx = mans_sinuss(x)
     print "%3d. a=%.5f f(%.5f)=%8.5f b=%.5f"%(k,a,x,funx,b)
-    if funa * funx > 0:
+    if funa *funx > 0:
         a = x
     else:
         b = x
-print "Rezultats: "
-print "a=%.9f(%.9f)=%12.9f b=%.9f"%(a,x,funx,b)
-print "Aprekins viekts ar %d interacijam"%(k)
+print "Rezultāts:"
+print "a=%.5f f(%.5f)=%12.9f b=%.9f"%(a,x,funx,b)
+print "Aprēķins veikts ar %d iterācijam"%(k)
